d1c446ca79dbb793f17533c31b753f13d0ccc92b
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-08-22 11:01:08 +0700

    add components no data row

--- resources/views/admin/eksplorasi_kerja/industri/section.blade.php
+++ resources/views/admin/eksplorasi_kerja/industri/section.blade.php
@@ -105,16 +105,8 @@
                             </td>
                         </tr>
                     @endforelse
-                    <tr id="noDataRow" class="hidden">
-                        <td colspan="6" class="text-center py-10">
-                            <div class="flex flex-col items-center justify-center space-y-3">
-                                <i class="fas fa-search text-gray-400 text-4xl"></i>
-                                <p class="text-gray-500 text-lg font-medium">
-                                    Tidak ada data yang sesuai dengan pencarian atau filter
-                                </p>
-                            </div>
-                        </td>
-                    </tr>
+                    
+                    <x-no-data-row />
                 </tbody>
             </table>
         </div>
