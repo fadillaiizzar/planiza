490389832eb4d703a744e166d0c161191cdafe0e
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-08-23 14:09:41 +0700

    add components search bar siswa

---
+++ resources/views/components/siswa/search-bar.blade.php
@@ -0,0 +1,7 @@
+<div class="mb-6 relative">
+    <i class="fas fa-magnifying-glass absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400"></i>
+    <input type="text"
+        id="{{ $id ?? 'search' }}"
+        placeholder="{{ $placeholder ?? 'Cari mata pelajaran atau materi' }}"
+        class="w-full sm:w-1/2 pl-10 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400">
+</div>
