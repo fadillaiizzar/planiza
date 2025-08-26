557c463f54c1c4580cd5959d9577a5d7e52f6139
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-08-26 15:01:38 +0700

    update return view kenali profesi admin

--- app/Http/Controllers/KenaliKerjaController.php
+++ app/Http/Controllers/KenaliKerjaController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class KenaliKerjaController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('admin.pages.kenali-karier');
+    }
 }
