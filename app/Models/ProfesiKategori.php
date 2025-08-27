88cdbae30e770c4eedbb86605b4b5e833848615e
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-08-27 09:48:14 +0700

    uptae model profesi kategori

--- app/Models/ProfesiKategori.php
+++ app/Models/ProfesiKategori.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProfesiKategori extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'profesi_kategoris';
+
+    protected $fillable = [
+        'profesi_kerja_id',
+        'kategori_minat_id',
+    ];
+
+    protected $with = ['profesiKerja', 'kategoriMinat'];
+
+    public function profesiKerja(): BelongsTo
+    {
+        return $this->belongsTo(ProfesiKerja::class, 'profesi_kerja_id');
+    }
+
+    public function kategoriMinat(): BelongsTo
+    {
+        return $this->belongsTo(KategoriMinat::class, 'kategori_minat_id');
+    }
 }
