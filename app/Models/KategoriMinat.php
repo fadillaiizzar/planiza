c0ab96dea1d6f8ac403699cdb2774764f23a93bb
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-08-27 09:48:04 +0700

    update model kategori minat

--- app/Models/KategoriMinat.php
+++ app/Models/KategoriMinat.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class KategoriMinat extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nama_kategori',
+        'deskripsi',
+    ];
+
+    public function profesiKerjas(): BelongsToMany
+    {
+        return $this->belongsToMany(ProfesiKerja::class, 'profesi_kategoris', 'kategori_minat_id', 'profesi_kerja_id');
+    }
 }
