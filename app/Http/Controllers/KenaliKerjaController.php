8ed2847a13da38b9b55922d2e6495bdad471c406
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-08-28 08:44:09 +0700

    add logic index kenali kerja admin

--- app/Http/Controllers/KenaliKerjaController.php
+++ app/Http/Controllers/KenaliKerjaController.php
@@ -2,12 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\ProfesiKerja;
 use Illuminate\Http\Request;
+use App\Models\KategoriMinat;
+use Illuminate\Support\Facades\Auth;
 
 class KenaliKerjaController extends Controller
 {
     public function index()
     {
-        return view('admin.pages.kenali-karier');
+        $kategoriMinat = KategoriMinat::latest()->get()->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'type' => 'Kategori Minat',
+                'name' => $item->nama_kategori,
+                'created_at' => $item->updated_at,
+                'action' => $item->created_at->eq($item->updated_at) ? 'create' : 'update',
+            ];
+        });
+
+        $profesiKerja = ProfesiKerja::latest()->get()->map(function ($item) {
+            return [
+                'id' => $item->id,
+                'type' => 'Profesi Kerja',
+                'name' => $item->nama_profesi_kerja,
+                'created_at' => $item->updated_at,
+                'action' => $item->created_at->eq($item->updated_at) ? 'create' : 'update',
+            ];
+        });
+
+        $activities = $kategoriMinat
+            ->merge($profesiKerja)
+            ->sortByDesc('created_at')
+            ->take(10);
+
+        $profesiCount = ProfesiKerja::count();
+        $kategoriMinatCount = KategoriMinat::count();
+
+        $user = Auth::user();
+        $userCount = User::count();
+
+        return view('admin.pages.kenali-profesi', [
+            'activities' => $activities,
+            'profesiCount' => $profesiCount,
+            'kategoriMinatCount' => $kategoriMinatCount,
+            'user' => $user,
+            'userCount' => $userCount,
+        ]);
     }
 }
