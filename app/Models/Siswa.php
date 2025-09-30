1230989b64a273093c1e4c203d3d759024eab050
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-09-30 11:26:01 +0700

    update models siswa

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -3,17 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Siswa extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'user_id', 
-        'kelas_id', 
-        'jurusan_id', 
+        'user_id',
+        'kelas_id',
+        'jurusan_id',
         'rencana_id',
         'no_hp',
     ];
@@ -23,18 +24,23 @@ class Siswa extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function kelas(): BelongsTo  
+    public function kelas(): BelongsTo
     {
         return $this->belongsTo(Kelas::class);
     }
 
-    public function jurusan(): BelongsTo  
+    public function jurusan(): BelongsTo
     {
         return $this->belongsTo(Jurusan::class);
     }
 
-    public function rencana(): BelongsTo  
+    public function rencana(): BelongsTo
     {
         return $this->belongsTo(Rencana::class);
+    }
+
+    public function jawabanSiswa(): HasMany
+    {
+        return $this->hasMany(JawabanSiswa::class, 'user_id', 'user_id');
     }
 }
