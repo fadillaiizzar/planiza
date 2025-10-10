c61e831f52f3c8f6e39db05f17a29e06e2e15e0a
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-10-10 14:41:40 +0700

    update link route breadcrumb

--- resources/views/admin/eksplorasi_kuliah/kampus_jurusan/kampus-jurusan.blade.php
+++ resources/views/admin/eksplorasi_kuliah/kampus_jurusan/kampus-jurusan.blade.php
@@ -9,7 +9,7 @@
     <!-- Main -->
     <main class="flex-1 min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 p-4 md:p-6">
         <x-admin.breadcrumb :links="[
-            ['href' => route('admin.eksplorasi-jurusan.kampus-jurusan.index'), 'icon' => 'fas fa-university', 'title' => 'Eksplorasi Kuliah'],
+            ['href' => route('admin.eksplorasi-jurusan.index'), 'icon' => 'fas fa-university', 'title' => 'Eksplorasi Kuliah'],
             ['href' => '#', 'icon' => 'fas fa-graduation-cap', 'title' => 'Kampus Jurusan'],
         ]" />
 
