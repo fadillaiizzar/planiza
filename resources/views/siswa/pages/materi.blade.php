5ad4051ac9a4a2fb74d4e6870047aa48376317f5
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-10-11 15:51:49 +0700

    update materi

--- resources/views/siswa/pages/materi.blade.php
+++ resources/views/siswa/pages/materi.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="min-h-screen bg-off-white">
-    <div class="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
+    <div class="mx-auto px-4 py-6 sm:px-6 lg:px-8">
 
         <x-siswa.section-header
             title="Temukan Materi"
