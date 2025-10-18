06534173a4849f6f38a1929475e5577c985de49e
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-10-18 10:06:03 +0700

    add false

--- resources/views/components/empty-state.blade.php
+++ resources/views/components/empty-state.blade.php
@@ -5,7 +5,7 @@
             <p class="text-gray-500 text-lg font-medium">
                 {{ $message ?? 'Belum ada data.' }}
             </p>
-            @if ($button)
+            @if ($button ?? false)
                 <button onclick="{{ $buttonAction ?? '' }}"
                     class="px-6 py-3 rounded-full font-semibold shadow-lg transition-all duration-300 border border-cool-gray hover:bg-cool-gray text-cool-gray hover:text-off-white hover:scale-105 focus:ring-4 focus:ring-cool-gray">
                     {{ $buttonText ?? '+ Tambah Data' }}
