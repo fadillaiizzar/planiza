2b1555ce99e370b56e957fc43410e6a1f8082d5d
Fadilla Izza <fadillaiizzarm@gmail.com>
2025-11-26 10:50:58 +0700

    update hubungan sdgs seeder

--- database/seeders/HubunganSdgsSeeder.php
+++ database/seeders/HubunganSdgsSeeder.php
@@ -17,75 +17,75 @@ class HubunganSdgsSeeder extends Seeder
         // ============================
         $profesiData = [
             // TKR
-            'Teknisi Otomotif' => [8, 9, 12],
-            'Operator Mesin Industri' => [8, 9, 12],
+            'Teknisi Otomotif' => [1, 8, 9, 12],
+            'Operator Mesin Industri' => [1, 8, 9, 12],
             'Teknisi Listrik Otomotif' => [7, 8, 9],
-            'Mekanik Motor' => [8, 12],
-            'Teknisi Kendaraan Berat' => [8, 9],
+            'Mekanik Motor' => [1, 8, 12],
+            'Teknisi Kendaraan Berat' => [1, 8, 9],
             'Teknisi Diagnosa Mesin' => [8, 9],
 
             // SIJA
             'Programmer' => [4, 8, 9, 17],
-            'Web Developer' => [4, 8, 9, 17],
-            'Network Engineer' => [9, 11, 17],
-            'System Analyst' => [4, 8, 9],
-            'Database Administrator' => [9, 16, 17],
-            'Cybersecurity Analyst' => [9, 16],
+            'Web Developer' => [4, 5, 8, 9, 17],
+            'Network Engineer' => [1, 9, 10, 11, 17],
+            'System Analyst' => [4, 8, 9, 10],
+            'Database Administrator' => [9, 10, 16, 17],
+            'Cybersecurity Analyst' => [9, 10, 16],
 
             // TAV
             'Teknisi Audio Visual' => [4, 8, 9],
-            'Video Editor' => [4, 8],
-            'Video Motion Specialist' => [4, 8],
+            'Video Editor' => [4, 5, 8, 10, 14],
+            'Video Motion Specialist' => [4, 5, 8, 14],
             'Sound Designer' => [4, 8],
             'Motion Graphics Designer' => [4, 8],
-            'Broadcast Technician' => [4, 8, 9],
+            'Broadcast Technician' => [4, 5, 8, 9, 14],
 
             // TITL
-            'Teknisi Telekomunikasi' => [7, 9, 11],
-            'Teknisi Fiber Optik' => [9, 11],
+            'Teknisi Telekomunikasi' => [1, 7, 9, 11],
+            'Teknisi Fiber Optik' => [3, 9, 11],
             'Operator BTS' => [9, 11, 16],
-            'Network Installer' => [9, 11],
+            'Network Installer' => [1, 3, 9, 10, 11],
             'Telecom Technician' => [9, 11],
             'Satellite Technician' => [9, 11, 17],
 
             // TP
-            'Operator Mesin Bubut' => [8, 9, 12],
-            'Operator Mesin CNC' => [8, 9, 12],
+            'Operator Mesin Bubut' => [1, 8, 9, 12],
+            'Operator Mesin CNC' => [1, 8, 9, 12],
             'Teknisi Produksi Manufaktur' => [8, 9, 12],
             'Teknisi Perawatan Mesin Industri' => [8, 9],
             'Quality Control Produk Mekanik' => [8, 9, 12],
             'Drafter Mesin' => [8, 9, 12],
 
             // DPIB
-            'Desainer Interior' => [8, 11, 12],
-            'Arsitek' => [9, 11, 12, 13],
+            'Desainer Interior' => [1, 8, 11, 12],
+            'Arsitek' => [1, 9, 11, 12, 13],
             'Desainer Furniture' => [8, 12],
             'Project Architect' => [9, 11, 13, 17],
-            '3D Visualizer' => [8, 11],
+            '3D Visualizer' => [1, 8, 11],
             'Desainer Retail' => [8, 12],
 
             // KGSP
             'Manajer Fasilitas Gedung' => [9, 11, 12, 13],
-            'Teknisi Pemeliharaan Bangunan' => [9, 11, 12],
+            'Teknisi Pemeliharaan Bangunan' => [6, 9, 11, 12],
             'Supervisor Konstruksi Gedung' => [8, 9, 11, 13],
-            'Ahli Sanitasi Bangunan' => [6, 11, 12],
-            'Teknisi Instalasi Gedung' => [7, 9, 11],
+            'Ahli Sanitasi Bangunan' => [3, 6, 11, 12],
+            'Teknisi Instalasi Gedung' => [1, 6, 7, 9, 10, 11],
             'Estimator Konstruksi' => [8, 9, 11],
 
             // DKV
-            'Desainer Grafis' => [4, 8, 12],
-            'Ilustrator' => [4, 8],
-            'Animator' => [4, 8],
+            'Desainer Grafis' => [4, 5, 8, 10, 12],
+            'Ilustrator' => [3, 4, 5, 8, 10, 14],
+            'Animator' => [3, 4, 5, 8],
             'Motion Designer' => [4, 8],
-            'Visual Storyteller' => [4, 8],
-            'Art Director' => [4, 8, 12, 17],
+            'Visual Storyteller' => [4, 5, 8],
+            'Art Director' => [4, 8, 10, 12, 17],
 
             // GEO
-            'Surveyor' => [9, 11, 13, 15],
+            'Surveyor' => [2, 9, 11, 13, 14, 15],
             'Geomatik' => [9, 11, 15, 17],
-            'Teknisi Pemetaan' => [9, 11, 15],
+            'Teknisi Pemetaan' => [2, 9, 11, 14, 15],
             'Cartographer' => [4, 9, 15],
-            'Remote Sensing Analyst' => [13, 14, 15],
+            'Remote Sensing Analyst' => [2, 13, 14, 15],
             'Ahli Sistem Informasi Geografis' => [9, 11, 13, 15, 17],
         ];
 
@@ -94,33 +94,33 @@ class HubunganSdgsSeeder extends Seeder
         // ============================
         $jurusanData = [
             // TKR
-            'Teknik Mesin Otomotif' => [8, 9, 12, 13],
-            'Teknik Otomotif' => [8, 9, 12],
-            'Teknik Elektro Otomotif' => [7, 8, 9, 12],
+            'Teknik Mesin Otomotif' => [1, 8, 9, 12, 13],
+            'Teknik Otomotif' => [1, 8, 9, 12],
+            'Teknik Elektro Otomotif' => [1, 7, 8, 9, 12],
 
             // SIJA
-            'Teknik Informatika' => [4, 8, 9, 11, 16, 17],
-            'Sistem Informasi' => [4, 8, 9, 16, 17],
-            'Teknologi Informasi' => [4, 8, 9, 11, 13, 17],
+            'Teknik Informatika' => [1, 2, 3, 4, 5, 8, 9, 10, 11, 16, 17],
+            'Sistem Informasi' => [1, 4, 5, 8, 9, 10, 16, 17],
+            'Teknologi Informasi' => [1, 3, 4, 8, 9, 11, 13, 17],
 
             // TAV
-            'Teknik Audio Video' => [4, 8, 9, 11],
-            'Film dan Televisi' => [4, 8, 11, 12],
-            'Seni Media Rekam' => [4, 8, 12],
+            'Teknik Audio Video' => [3, 4, 6, 8, 9, 11],
+            'Film dan Televisi' => [3, 4, 5, 8, 10, 11, 12, 14],
+            'Seni Media Rekam' => [3, 4, 5, 8, 10, 12, 14],
 
             // TITL
-            'Teknik Elektro' => [7, 9, 11, 12],
+            'Teknik Elektro' => [1, 2, 3, 6, 7, 9, 10, 11, 12],
             'Teknik Telekomunikasi' => [7, 9, 11, 16, 17],
-            'Teknik Komputer' => [4, 8, 9, 11, 16, 17],
+            'Teknik Komputer' => [1, 4, 6, 8, 9, 10, 11, 16, 17],
 
             // DKV
-            'Desain Komunikasi Visual' => [4, 8, 12, 17],
-            'Animasi' => [4, 8, 11, 12],
-            'Desain Produk' => [8, 9, 12, 13],
+            'Desain Komunikasi Visual' => [3, 4, 5, 8, 10, 12, 17],
+            'Animasi' => [3, 4, 5, 8, 10, 11, 12, 14],
+            'Desain Produk' => [3, 5, 8, 9, 12, 13],
 
             // DPIB
-            'Arsitektur' => [9, 11, 12, 13],
-            'Desain Interior' => [8, 11, 12],
+            'Arsitektur' => [1, 3, 6, 9, 11, 12, 13],
+            'Desain Interior' => [1, 3, 6, 8, 11, 12],
             'Teknologi Rekayasa Konstruksi' => [9, 11, 12, 13],
 
             // KGSP
